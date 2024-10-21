47175897e22c560381d2721902af6519167f2bf4
Stéphanie Vanderhaegen <steph@Buddy.eduroam.thomasmore.be>
2024-10-21 15:05:02 +0200

    added env

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // Importeren van de express module in node_modules
 const express = require('express');
 
+const Database = require('./classes/database.js')
+
 // Aanmaken van een express app
 const app = express();
+
+const db = new Database();
+db.getQuery('SELECT * FROM artists').then((rows) => {
+    console.log(rows);
+});
 
 // Endpoints
 app.get('/', (req, res) => {
@@ -10,10 +17,26 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/artists', (req, res) => {
-    res.send([
-        "JB",
-        "Beyonce",
-    ]);
+    const db = new Database();
+    db.getQuery('SELECT * FROM artists').then((artists) => {
+        res.send(artists);
+    });
+});
+
+app.get('/api/songs', (req, res) => {
+    const db = new Database();
+    db.getQuery(`
+        SELECT
+          song_id, s.name AS songname, a.name AS artistname
+            FROM
+                songs AS s
+                    INNER JOIN
+                        artists AS a
+                            ON
+                                s.artist_id = a.artist_id;
+        `).then((songs) => {
+            res.send(songs);
+    });
 });
 
 
