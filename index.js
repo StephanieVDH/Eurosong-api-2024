45acf2fcfa7f88d1c78a11c0bd1004bbd8de79f8
Stéphanie Vanderhaegen <steph@Buddy.eduroam.thomasmore.be>
2024-10-21 12:30:15 +0200

    added api/artists endpoint

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
     res.send('Hello world!');
 });
 
+app.get('/api/artists', (req, res) => {
+    res.send([
+        "JB",
+        "Beyonce",
+    ]);
+});
+
+
 // Starten van de server en op welke port de server moet luisteren
 app.listen(3000, () => {
     console.log('Server is running port 3000');
